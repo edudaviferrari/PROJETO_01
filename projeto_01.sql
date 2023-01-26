fe0eb1ed0b26566c0d098684be90f4ab5262d9c9
Eduardo Ferrari <edudfj@gmail.com>
2023-01-26 14:37:51 -0300

    Banco de Dados

--- projeto_01.sql
+++ projeto_01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Jan-2023 às 20:33
+-- Tempo de geração: 26-Jan-2023 às 18:37
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.1.12
 
@@ -24,25 +24,67 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tb_admin.online`
+--
+
+CREATE TABLE `tb_admin.online` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `ultima_acao` datetime NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tb_admin.usuarios`
 --
 
 CREATE TABLE `tb_admin.usuarios` (
   `id` int(11) NOT NULL,
   `user` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cargo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `tb_admin.usuarios`
 --
 
-INSERT INTO `tb_admin.usuarios` (`id`, `user`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `tb_admin.usuarios` (`id`, `user`, `password`, `img`, `nome`, `cargo`) VALUES
+(1, 'admin', 'admin', 'foto.jpg', 'Eduardo Ferrari', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.visitas`
+--
+
+CREATE TABLE `tb_admin.visitas` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `dia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `tb_admin.visitas`
+--
+
+INSERT INTO `tb_admin.visitas` (`id`, `ip`, `dia`) VALUES
+(1, '::1', '2023-01-26'),
+(2, '::1', '2023-01-26');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `tb_admin.usuarios`
@@ -51,14 +93,32 @@ ALTER TABLE `tb_admin.usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `tb_admin.usuarios`
 --
 ALTER TABLE `tb_admin.usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
